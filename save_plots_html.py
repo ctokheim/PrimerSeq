d56b167f131ddd806a8eb0de27bb3fa1f39e48d9
ctokheim <ctokheim@example.com>
2013-04-10 15:37:34 -0700

    Changed version number within software to 1.1.1.beta

--- save_plots_html.py
+++ save_plots_html.py
@@ -55,7 +55,7 @@ class SavePlotsHTML(object):
 <html>
 <head>
     <LINK href="%s" rel="stylesheet" type="text/css">
-    <title>PrimerSeq v1.1.0.beta Output</title>
+    <title>PrimerSeq v1.1.1.beta Output</title>
 </head>
 <body>
 <div class='container'>
