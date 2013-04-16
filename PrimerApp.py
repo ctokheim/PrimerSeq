58ca5968a4c947e09dbcc601b4f1cea559c09b8b
ctokheim <ctokheim@example.com>
2013-04-16 16:13:51 -0700

    Changed version number within software to v1.1.2.beta

--- PrimerApp.py
+++ PrimerApp.py
@@ -694,7 +694,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>."""
         tmp_icon = wx.EmptyIcon()
         tmp_icon.CopyFromBitmap(wx.Bitmap("about.ico", wx.BITMAP_TYPE_ANY))
         info.SetName('PrimerSeq')
-        info.SetVersion('1.1.1.beta')
+        info.SetVersion('1.1.2.beta')
         info.SetDescription(description)
         info.SetCopyright('(C) 2012-2013 Collin Tokheim')
         info.SetWebSite('http://primerseq.sf.net')
