17475cc2269f7de4752eddeb282892b85fe64177
ctokheim <ctokheim@example.com>
2013-12-18 11:35:32 -0500

    Updated version number in software to v1.1.8

--- PrimerApp.py
+++ PrimerApp.py
@@ -821,7 +821,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>."""
         tmp_icon = wx.EmptyIcon()
         tmp_icon.CopyFromBitmap(wx.Bitmap("about.ico", wx.BITMAP_TYPE_ANY))
         info.SetName('PrimerSeq')
-        info.SetVersion('1.1.7.beta')
+        info.SetVersion('1.1.8')
         info.SetDescription(description)
         info.SetCopyright('(C) 2012-2013 Collin Tokheim')
         info.SetWebSite('http://primerseq.sf.net')
--- save_plots_html.py
+++ save_plots_html.py
@@ -55,7 +55,7 @@ class SavePlotsHTML(object):
 <html>
 <head>
     <LINK href="%s" rel="stylesheet" type="text/css">
-    <title>PrimerSeq v1.1.7.beta Output</title>
+    <title>PrimerSeq v1.1.8 Output</title>
 </head>
 <body>
 <div class='container'>
